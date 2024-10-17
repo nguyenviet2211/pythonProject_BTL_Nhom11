dc16a061cc935d81879291333ba7b9ae4238fddd
nguyen viet <167691410+nguyenviet2211@users.noreply.github.com>
2024-10-17 22:53:12 +0700

    viet 17/10/2024

--- character.py
+++ character.py
@@ -1,49 +1,46 @@
 import pygame
 
 
-def sprite_list(sprite, n, width, height):
-    return [pygame.transform.scale(sprite.subsurface((i * width, 0), (width, height)), (126, 126)) for i in range(n)]
+def sprite_list(sprite, num_frame):
+    width = sprite.get_width()/num_frame
+    height = sprite.get_height()
+    n = int(width/height)
+    return [pygame.transform.scale(
+        sprite.subsurface((i * width, 0), (width, height)), (126, 126)) for i in range(num_frame)]
 
 
 def image(Image):
     return pygame.image.load(Image)
 
 
-def create_char(i, key, x, ground):
-    sprite_sheet_attack = image(f'Data/character/char{i}/Attack2.png')
+def create_char(i, key, x, ground, num_frame):
+    sprite_sheet_attack = image(f'Data/character/char{i}/attack.png')
     sprite_sheet_run = image(f'Data/character/char{i}/run.png')
     sprite_sheet_jump = image(f'Data/character/char{i}/jump.png')
     sprite_sheet_idle = image(f'Data/character/char{i}/idle.png')
-    sprite_sheet_run_attack = image(f'Data/character/char{i}/RunAttack2.png')
-    sprite_sheet_squat_attack = image(f'Data/character/char{i}/SquatAttack.png')
-    sprite_sheet_walk_attack = image(f'Data/character/char{i}/WalkAttack1.png')
+    sprite_sheet_run_attack = image(f'Data/character/char{i}/RunAttack.png')
     sprite_sheet_die = image(f'Data/character/char{i}/Death.png')
 
-    frame_width = 42
-    frame_height = 42
+    attack = sprite_list(sprite_sheet_attack, num_frame[0])
+    run = sprite_list(sprite_sheet_run, num_frame[1])
+    jump = sprite_list(sprite_sheet_jump, num_frame[2])
+    idle = sprite_list(sprite_sheet_idle, num_frame[3])
+    run_attack = sprite_list(sprite_sheet_run_attack, num_frame[4])
+    death = sprite_list(sprite_sheet_die, num_frame[5])
 
-    attack = sprite_list(sprite_sheet_attack, 6, frame_width, frame_height)
-    run = sprite_list(sprite_sheet_run, 6, frame_width, frame_height)
-    jump = sprite_list(sprite_sheet_jump, 8, frame_width, frame_height)
-    idle = sprite_list(sprite_sheet_idle, 4, frame_width, frame_height)
-    run_attack = sprite_list(sprite_sheet_run_attack, 6, frame_width, frame_height)
-    squat_attack = sprite_list(sprite_sheet_squat_attack, 6, frame_width, frame_height)
-    walk_attack = sprite_list(sprite_sheet_walk_attack, 6, frame_width, frame_height)
-    death = sprite_list(sprite_sheet_die, 9, frame_width, frame_height)
-
-    sprite = Character(key, x,  ground, attack, run, jump, idle, run_attack, squat_attack, walk_attack, death)
+    sprite = Character(num_frame, key, x,  ground, attack, run, jump, idle, run_attack, death)
     return sprite
 
 
 def update_counter(counter, num_frame) -> int:
     counter += 1
-    if counter > num_frame:
+    if counter == num_frame:
         return 0
     return counter
 
 
 class Character(pygame.sprite.Sprite):
-    def __init__(self, key, x, ground,  *frame):
+    def __init__(self, num_frame, key, x, ground,  *frame):
         super().__init__()
         # frame
         self.attack_frame = frame[0]
@@ -51,9 +48,7 @@ class Character(pygame.sprite.Sprite):
         self.jump_frame = frame[2]
         self.idle_frame = frame[3]
         self.run_attack_frame = frame[4]
-        self.squat_frame = frame[5]
-        self.walk_attack_frame = frame[6]
-        self.death = frame[7]
+        self.death = frame[5]
         self.image = self.idle_frame[0]
         self.rect = self.image.get_rect()
         self.animation_speed = 4
@@ -62,7 +57,6 @@ class Character(pygame.sprite.Sprite):
         self.run_Counter = 0
         self.jump_counter = 0
         self.idle_counter = 0
-        self.squat_counter = 0
         self.fall_attack_counter = 0
         self.death_counter = 0
         # Key
@@ -76,6 +70,9 @@ class Character(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = ground
         self.ground = ground
+        self.num_frame = num_frame
+        self.animation_speed = 10
+        self.width = pygame.display.get_desktop_sizes()[0][0] - 226
 
     def update_image(self, frame, counter):
         if self.prev_key != self.key[1]:
@@ -88,7 +85,7 @@ class Character(pygame.sprite.Sprite):
             self.rect.x = max(0, self.rect.x - dis)
             self.prev_key = self.key[1]
         elif key[self.key[2]]:
-            self.rect.x = min(1300 - 126, self.rect.x + dis)
+            self.rect.x = min(self.width, self.rect.x + dis)
             self.prev_key = self.key[2]
 
     def update(self):
@@ -99,13 +96,9 @@ class Character(pygame.sprite.Sprite):
                 self.update_image(self.jump_frame, self.jump_counter)
                 self.rect.y = self.ground - self.jump_counter * (7 - self.jump_counter) * 15
                 self.update_prev_key(key, 15)
-                self.jump_counter = update_counter(self.jump_counter, 7)
-                pygame.time.delay(25)
+                self.jump_counter = update_counter(self.jump_counter, self.num_frame[2])
+                pygame.time.delay(35)
             # squat attack
-            elif key[self.key[5]]:
-                self.update_image(self.squat_frame, self.squat_counter)
-                self.update_prev_key(key, 15)
-                self.squat_counter = update_counter(self.squat_counter, 5)
             # run
             elif key[self.key[1]] or key[self.key[2]]:
                 if key[self.key[3]]:
@@ -113,27 +106,26 @@ class Character(pygame.sprite.Sprite):
                 else:
                     self.update_image(self.run_frame, self.run_Counter)
                 self.update_prev_key(key, 15)
-                self.run_Counter = update_counter(self.run_Counter, 5)
+                self.run_Counter = update_counter(self.run_Counter, self.num_frame[1])
             # attack
             elif key[self.key[3]]:
                 self.update_image(self.attack_frame, self.attack_counter)
-                self.attack_counter = update_counter(self.attack_counter, 5)
+                self.attack_counter = update_counter(self.attack_counter, self.num_frame[0])
             # idle
             else:
                 self.update_image(self.idle_frame, self.idle_counter)
-                self.idle_counter = update_counter(self.idle_counter, 3)
-        elif self.death_counter < 9:
+                self.idle_counter = update_counter(self.idle_counter, self.num_frame[3])
+        elif self.death_counter < self.num_frame[5]:
             self.update_image(self.death, self.death_counter)
             self.death_counter += 1
             pygame.time.delay(50)
         else:
-            self.kill()
             self.alive = False
 
     def fight(self, char2):
         key = pygame.key.get_pressed()
         if pygame.sprite.collide_mask(self, char2):
-            if (key[pygame.K_j] or key[pygame.K_s]) and self.alive:
+            if (key[pygame.K_j]) and self.alive:
                 char2.health -= self.base_atk
-            if (key[pygame.K_KP1] or key[pygame.K_DOWN]) and char2.alive:
+            if (key[pygame.K_KP1]) and char2.alive:
                 self.health -= char2.base_atk
--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import character
 import threading
 import sys
 
+
+def load_image(image, w, h):
+    return pygame.transform.scale(pygame.image.load(image).convert_alpha(), (w, h))
+
+
 pygame.init()
 clock = pygame.time.Clock()
 x1, x2, x3 = 0, 0, 0
@@ -10,48 +15,58 @@ color1, color2 = (255, 255, 255), (100, 100, 100)
 Font = pygame.font.SysFont("Press Start 2P", 40)
 key_player_1 = [pygame.K_d, pygame.K_a, pygame.K_d, pygame.K_j, pygame.K_w, pygame.K_s]
 key_player_2 = [pygame.K_LEFT, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_KP1, pygame.K_UP, pygame.K_DOWN]
-Background_info = [360, 500]
 screen_width, screen_height = (pygame.display.get_desktop_sizes()[0][0] - 100,
                                pygame.display.get_desktop_sizes()[0][1] - 100)
+Background_info = [0.54, 0.75]
 screen = pygame.display.set_mode((screen_width, screen_height))
-
-
-def load_image(image, w, h):
-    return pygame.transform.scale(pygame.image.load(image).convert_alpha(), (w, h))
-
-
-def black_blur():
-    blur = pygame.Surface((screen_width, screen_height))
-    blur.fill((0, 0, 0))
-    blur.set_alpha(120)
-    screen.blit(blur, (0, 0))
-    blur.set_alpha(None)
 
 
 # load data
 Background = [load_image(f"Data/Background/Background{i}.jpg", screen_width, screen_height) for i in range(2)]
+Background_2p_mode = [load_image(f"Data/Background/Background{i}.jpg", 300, 150) for i in range(2)]
 Menu = load_image("Data/Background3.jpg", screen_width, screen_height)
 Button = load_image("Data/game_button/buttons-3.png", 200, 50)
 left_arrow = load_image("Data/game_button/buttons-4.png", 35, 50)
 right_arrow = load_image("Data/game_button/buttons-6.png", 35, 50)
-Char = [load_image(f"Data/character/char{i}/avatar.png", 100, 100) for i in range(3)]
-name_character = ["monster", "rabbit", "snow man"]
+avatar_char = [load_image(f"Data/character/char{i}/avatar.png", 100, 100) for i in range(4)]
+name_character = ["monster", "rabbit", "snow man", "naruto"]
+NumFrame = [
+    [6, 6, 8, 4, 6, 9],
+    [6, 6, 8, 4, 6, 9],
+    [6, 6, 8, 4, 6, 9],
+    [10, 6, 5, 6, 10, 6],
+]
+idle_char = [character.sprite_list(pygame.image.load(f"Data/character/char{i}/idle.png"), NumFrame[i][3])
+             for i in range(4)]
+idle_count = [0 for i in range(4)]
 
 
-def draw_text(x, y, Text, button=None, TextColor=(0, 0, 0), bg_color=None):
+def Blur(color):
+    blur = pygame.Surface((screen_width, screen_height))
+    blur.fill(color)
+    blur.set_alpha(120)
+    screen.blit(blur, (0, 0))
+    return blur
+
+
+def draw_text(x, y, Text, button=None, TextColor=(255, 255, 255), bg_color=None):
     text = Font.render(Text, True, TextColor)
     rect = textRect = text.get_rect()
     textRect.center = (x, y)
     if bg_color is not None:
-        pygame.draw.rect(screen, bg_color, rect)
+        bg_rect = pygame.Rect(0, 0, 200, 40)
+        bg_rect.center = (x, y)
+        pygame.draw.rect(screen, bg_color, bg_rect)
+
     if button is not None:
         rect = button.get_rect()
         rect.center = (x, y)
         screen.blit(button, rect)
+
     screen.blit(text, textRect)
     if button is not None:
         return rect
-    return textRect
+    return rect
 
 
 def draw_health_bar(x, y, char):
@@ -66,6 +81,9 @@ def draw_char(sprite, x, i, char):
     draw_health_bar(x - textRect.width / 2, 90, char)
 
 
+black_blur = Blur((0, 0, 0))
+
+
 def draw_arrow():
     left_arrow_char1 = draw_text(screen_width / 5 - 50, screen_height / 4 * 3, "", left_arrow)
     right_arrow_char1 = draw_text(screen_width / 5 + 50, screen_height / 4 * 3, "", right_arrow)
@@ -78,13 +96,12 @@ def draw_arrow():
 
 
 def menu():
-    pygame.display.flip()
     pygame.display.set_caption("2P game")
     color = [color1, color1, color1]
 
     Exit = False
     while not Exit:
-        screen.blit(Menu, (0, 0))
+        screen.blit(pygame.transform.scale(Menu, (screen_width, screen_height)), (0, 0))
         start_button = draw_text(screen_width / 2, screen_height / 2 - 60, "START", Button, color[0])
         _2P_ = draw_text(screen_width / 2, screen_height / 2, "2P", Button, color[1])
         exit_button = draw_text(screen_width / 2, screen_height / 2 + 60, "EXIT", Button, color[2])
@@ -123,56 +140,54 @@ def _2p_mode():
     rect2.center = (screen_width / 5 * 4, screen_height / 2)
     rect_background.center = (screen_width / 2, screen_height / 2)
     global x1, x2, x3
-    color = color1
-    list_arrow = draw_arrow()
     Exit = False
     while not Exit:
         screen.fill((0, 0, 0))
         draw_text(screen_width / 2, screen_height / 4, "Map", None, color1)
         draw_text(screen_width / 5, screen_height / 4, name_character[x1], None, color1)
         draw_text(screen_width / 5 * 4, screen_height / 4, name_character[x2], None, color1)
-        draw_arrow()
-        start = draw_text(screen_width / 2, screen_height - 100, "START", Button, color)
+        start = draw_text(screen_width / 2, screen_height - 100, "START", Button)
+        list_arrow = draw_arrow()
         mouse = pygame.mouse.get_pos()
         screen.blits((
-            (Char[x1], rect1),
-            (Char[x2], rect2),
-            (pygame.transform.scale(Background[x3], (300, 150)), rect_background))
+            (idle_char[x1][idle_count[x1]], rect1),
+            (idle_char[x2][idle_count[x2]], rect2),
+            (Background_2p_mode[x3], rect_background))
         )
-        pygame.event.set_allowed([pygame.MOUSEBUTTONDOWN, pygame.MOUSEBUTTONUP, pygame.QUIT])
         for even in pygame.event.get():
             if even.type == pygame.QUIT:
                 Exit = True
+                break
+
             if start.collidepoint(mouse):
-                color = color2
                 if even.type == pygame.MOUSEBUTTONUP:
                     play_game(Background[x3])
-                    screen.fill(color1)
-                    draw_arrow()
-            else:
-                color = color1
 
             if even.type == pygame.MOUSEBUTTONDOWN:
                 if list_arrow[0].collidepoint(mouse):
-                    x1 = (x1 + 2) % 3
+                    x1 = (x1 + 3) % 4
                 if list_arrow[1].collidepoint(mouse):
-                    x2 = (x2 + 2) % 3
+                    x2 = (x2 + 3) % 4
                 if list_arrow[2].collidepoint(mouse):
-                    x1 = (x1 + 1) % 3
+                    x1 = (x1 + 1) % 4
                 if list_arrow[3].collidepoint(mouse):
-                    x2 = (x2 + 1) % 3
+                    x2 = (x2 + 1) % 4
                 if list_arrow[4].collidepoint(mouse):
                     x3 = (x3 + 1) % 2
                 if list_arrow[5].collidepoint(mouse):
                     x3 = (x3 + 1) % 2
 
+        idle_count[x1] = character.update_counter(idle_count[x1], NumFrame[x1][3])
+        idle_count[x2] = character.update_counter(idle_count[x2], NumFrame[x2][3])
+        clock.tick(12)
         pygame.display.update()
 
 
 def play_game(background):
     # load character
-    char1 = character.create_char(x1, key_player_1, 100, Background_info[x3])
-    char2 = character.create_char(x2, key_player_2, 1100, Background_info[x3])
+    char1 = character.create_char(x1, key_player_1, 50, int(Background_info[x3] * screen_height), NumFrame[x1])
+    char2 = character.create_char(x2, key_player_2, screen_width - 150, int(Background_info[x3] * screen_height),
+                                  NumFrame[x2])
     Group1 = pygame.sprite.Group(char1)
     Group2 = pygame.sprite.Group(char2)
     e = False
@@ -182,17 +197,17 @@ def play_game(background):
                 e = True
         screen.blit(pygame.transform.scale(background, (screen_width, screen_height)), (0, 0))
         p1 = threading.Thread(target=draw_char(Group1, 100, 1, char1))
-        p2 = threading.Thread(target=draw_char(Group2, 1100, 2, char2))
+        p2 = threading.Thread(target=draw_char(Group2, screen_width - 200, 2, char2))
         p1.start()
         p2.start()
         p1.join()
         p2.join()
         char1.fight(char2)
         if not char1.alive or not char2.alive:
-            black_blur()
+            screen.blit(black_blur, (0, 0))
             draw_text(screen_width / 2, screen_height / 2, "GAME OVER", None, (255, 255, 255))
         pygame.display.update()
-        clock.tick(30)  # FPS
+        clock.tick(24)  # FPS
 
 
 if __name__ == '__main__':
